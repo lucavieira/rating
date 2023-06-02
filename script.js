1f825c8a07a31329392a2c437369f64ba776d6a9
lucavieira <lukasveras14@gmail.com>
2023-06-02 01:54:33 -0300

    :hammer: Action to Submit Button add

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let lista = document.getElementById('container-test').children
+let submitButton = document.getElementById('submit-button')
+let ratingList = document.getElementById('container-test').children
+let ratingSelected = document.getElementById('rating-selected')
+let ratingState = document.getElementById('state-1')
+let stateThank = document.getElementById('state-2')
+let ratingButton
 
 function selectRating (id) {
-    let ratingButton = document.getElementById(id)
-    for(element of lista) {
+    ratingButton = document.getElementById(id)
+    for(element of ratingList) {
         if(element.classList.contains('selected')) {
-            let ratingButtonTemporary = document.getElementById(element.id)
-            ratingButtonTemporary.classList.remove('selected')
+            let ratingButtonAux = document.getElementById(element.id)
+            ratingButtonAux.classList.remove('selected')
         }
     }
     ratingButton.classList.toggle('selected')
 }
+
+submitButton.addEventListener('click', () => {
+    if (ratingButton == undefined) {
+        alert('Escolha uma nota para avaliação!')
+    } else {
+        ratingState.style.display = 'none'
+        stateThank.style.display = 'flex'
+        ratingSelected.innerHTML = ratingButton.id
+    }
+})
